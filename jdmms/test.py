a11f5ae307fc02d36a991f793d14b03e3aed6ce8
Mikepeng <mikepeng@MikepengdeMacBook-Pro.local>
2018-01-29 23:42:43 +0800

    deleted:    jdmms/mms.py 	deleted:    jdmms/mms_client.py 	deleted:    jdmms/test.py

--- jdmms/test.py
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/python
-from suds.client import Client 
-
-client = Client('http://mms.360buy.com/services/NewMessageServer?wsdl=NewMessageSender.wsdl')
-print client
-
-result =  client.service.getMobileCodeInfo(18688950910)
-print result
-print client.last_received()
